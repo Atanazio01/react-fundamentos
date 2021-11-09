c5f23de2774432f93ec0c2fa3bba5676c046ca50
Atanazio01 <marcosd.atanazio@gmail.com>
2021-11-09 18:03:51 -0300

    Working with inline styles

--- src/Header.js
+++ src/Header.js
@@ -7,13 +7,18 @@ import { ThemeContext } from './ThemeContext';
 export default function Header(props) {
   const { onToggleTheme } = useContext(ThemeContext);
   return (
-    <>
+    <div
+      style={{
+        background: '#ccc',
+        fontFamily: 'sans-serif',
+      }}
+    >
       <h1>{props.title}</h1>
       <Button onClick={onToggleTheme}>
         Mudar tema
       </Button>
       {props.children}
-    </>
+    </div>
   );
 }
 
