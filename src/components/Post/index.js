de985a401c3a249f3ae3408824a909dfb1409d86
Atanazio01 <marcosd.atanazio@gmail.com>
2021-11-09 20:21:03 -0300

    Creating styled components

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import PostHeader from './PostHeader';
 
-import styles from './styles.scss'
+import PostHeader from './PostHeader';
+import styles from './styles.scss';
+
+import { Subtitle, Rate } from './styles';
 
 export default function Post(props) {
 
@@ -23,10 +25,8 @@ export default function Post(props) {
             read: props.post.read,
           }}
         />
-        <br />
-        <small>{props.post.subtitle}</small>
-        <br />
-        Média: {props.post.likes / 2}
+        <Subtitle>{props.post.subtitle}</Subtitle>
+        <Rate>Média: {props.post.likes / 2}</Rate>
       </article>
   );
 };
